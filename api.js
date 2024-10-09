4d0690b84b33aed6cfac1877094f63e2f296dd47
Mr. Error <mr.101rror@gmail.com>
2024-10-10 02:32:31 +0600

    Bug Fix

--- api.js
+++ api.js
@@ -1,151 +1,143 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyP = require("body-parser");
-const compiler = require("compilex");
-const options = { stats: true };
-const fs = require("fs");
+const bodyP = require('body-parser');
+const compiler = require('compilex');
+const options = {
+  stats: true
+};
+const fs = require('fs');
 compiler.init(options);
 
 app.use(bodyP.json());
-app.use("/codemirror-5.65.17", express.static("G:/CodeAura/codemirror-5.65.17"));
+app.use(
+    '/codemirror-5.65.17', express.static('G:/CodeAura/codemirror-5.65.17'));
 
 
 
-
-app.get("/", function (req, res) {
-    compiler.flush(function () {
-        console.log("deleted");
-    })
-    res.sendFile("G:/CodeAura/index.html");
+app.get('/', function(req, res) {
+  compiler.flush(function() {
+    console.log('deleted');
+  })
+  res.sendFile('G:/CodeAura/index.html');
 });
 
 
 
+app.post('/compile', function(req, res) {
+  var code = req.body.code;
+  var input = req.body.input;
+  var lang = req.body.lang;
 
-app.post("/compile", function (req, res) {
-    var code = req.body.code;
-    var input = req.body.input;
-    var lang = req.body.lang;
-
-    try {
-        if (lang === "Cpp") {
-            if (!input) {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
-                compiler.compileCPP(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" });
-                    }
-                });
-            }
-            else {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
-                compiler.compileCPPWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" });
-                    }
-                });
-            }
-        }
-        else if (lang === "Java") {
-            if (!input) {
-                var envData = { OS: "windows" };
-                compiler.compileJava(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" });
-                    }
-                });
-            }
-            else {
-                var envData = { OS: "windows" };
-                compiler.compileJavaWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" });
-                    }
-                });
-            }
-        }
-        else if (lang === "Python") {
-            if (!input) {
-                var envData = { OS: "windows" };
-                compiler.compilePython(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" });
-                    }
-                });
-            }
-            else {
-                var envData = { OS: "windows" };
-                compiler.compilePythonWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" });
-                    }
-                });
-            }
-        }
+  try {
+    if (lang === 'Cpp') {
+      if (!input) {
+        var envData = {OS: 'windows', cmd: 'g++', options: {timeout: 10000}};
+        compiler.compileCPP(envData, code, function(data) {
+          if (data.output) {
+            res.send(data);
+          } else {
+            res.send({output: 'error'});
+          }
+        });
+      } else {
+        var envData = {OS: 'windows', cmd: 'g++', options: {timeout: 10000}};
+        compiler.compileCPPWithInput(envData, code, input, function(data) {
+          if (data.output) {
+            res.send(data);
+          } else {
+            res.send({output: 'error'});
+          }
+        });
+      }
+    } else if (lang === 'Java') {
+      if (!input) {
+        var envData = {OS: 'windows'};
+        compiler.compileJava(envData, code, function(data) {
+          if (data.output) {
+            res.send(data);
+          } else {
+            res.send({output: 'error'});
+          }
+        });
+      } else {
+        var envData = {OS: 'windows'};
+        compiler.compileJavaWithInput(envData, code, input, function(data) {
+          if (data.output) {
+            res.send(data);
+          } else {
+            res.send({output: 'error'});
+          }
+        });
+      }
+    } else if (lang === 'Python') {
+      if (!input) {
+        var envData = {OS: 'windows'};
+        compiler.compilePython(envData, code, function(data) {
+          if (data.output) {
+            res.send(data);
+          } else {
+            res.send({output: 'error'});
+          }
+        });
+      } else {
+        var envData = {OS: 'windows'};
+        compiler.compilePythonWithInput(envData, code, input, function(data) {
+          if (data.output) {
+            res.send(data);
+          } else {
+            res.send({output: 'error'});
+          }
+        });
+      }
     }
-    catch (e) {
-        console.log("Error" + e.message);
-    }
+  } catch (e) {
+    console.log('Error' + e.message);
+  }
 });
 
 
 
 // For Cpp
-app.get("/template/cpp", function (req, res) {
-    fs.readFile("G:/CodeAura/templates/cpp_template.txt", "utf8", function (err, data) {
+app.get('/template/cpp', function(req, res) {
+  fs.readFile(
+      'G:/CodeAura/templates/cpp_template.txt', 'utf8', function(err, data) {
         if (err) {
-            res.status(500).send("Template not found");
+          res.status(500).send('Template not found');
+        } else {
+          res.send({template: data});
         }
-        else {
-            res.send({ template: data });
-        }
-    });
+      });
 });
 
 
 
-//For Java
-app.get("/template/java", function (req, res) {
-    fs.readFile("G:/CodeAura/templates/java_template.txt", "utf8", function (err, data) {
+// For Java
+app.get('/template/java', function(req, res) {
+  fs.readFile(
+      'G:/CodeAura/templates/java_template.txt', 'utf8', function(err, data) {
         if (err) {
-            res.status(500).send("Template not found");
+          res.status(500).send('Template not found');
+        } else {
+          res.send({template: data});
         }
-        else {
-            res.send({ template: data });
-        }
-    });
+      });
 });
 
 
 
-//For Python
-app.get("/template/python", function (req, res) {
-    fs.readFile("G:/CodeAura/templates/python_template.txt", "utf8", function (err, data) {
+// For Python
+app.get('/template/python', function(req, res) {
+  fs.readFile(
+      'G:/CodeAura/templates/python_template.txt', 'utf8', function(err, data) {
         if (err) {
-            res.status(500).send("Template not found");
+          res.status(500).send('Template not found');
+        } else {
+          res.send({template: data});
         }
-        else {
-            res.send({ template: data });
-        }
-    });
+      });
 });
 
-app.listen(8000);
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
